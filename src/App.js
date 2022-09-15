4e93d3d86fe3b2b8ebe53cb850a5f8ba1e5065e2
monet2155 <monet9812@gmail.com>
2022-09-15 16:32:23 +0900

    feat: move robot to target

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import './App.css'
 
 function App() {
   
-  const {unityProvider, isLoaded} = useUnityContext({
-    loaderUrl: "Build/build_ver0.1.loader.js",
-    dataUrl: "Build/build_ver0.1.data",
-    frameworkUrl: "Build/build_ver0.1.framework.js",
-    codeUrl: "Build/build_ver0.1.wasm",
+  const {unityProvider, isLoaded, sendMessage} = useUnityContext({
+    loaderUrl: "Build/build_ver0.2.loader.js",
+    dataUrl: "Build/build_ver0.2.data",
+    frameworkUrl: "Build/build_ver0.2.framework.js",
+    codeUrl: "Build/build_ver0.2.wasm",
   })
 
   const [hideSplash, setHideSplash] = useState(true);
@@ -24,12 +24,8 @@ function App() {
     }
   }, [isLoaded])
 
-  const onClickPoiButton = (x, z) => {
-    
-  }  
-
-  const onClickCustomPositionButton = () => {
-
+  const handleRobotPosition = (x,z) => {
+    sendMessage('Robot', "setDestination" , `${x},${z}`)
   }
 
   return (
@@ -46,15 +42,15 @@ function App() {
           <h1>Utility</h1>
           <h2>1. 이동</h2>
           <h3>지점</h3>
-          <button onClick={() => onClickPoiButton(0, 0)}>x:0 / y:0</button>
-          <button onClick={() => onClickPoiButton(2, 3)}>x:2 / y:3</button>
+          <button onClick={() => handleRobotPosition(0, 0)}>x:0 / y:0</button>
+          <button onClick={() => handleRobotPosition(0, 10)}>x:0 / y:10</button>
           <h3>직접 지정</h3>
           <div style={{marginTop: '10px', display: 'flex',flexDirection: 'column'}}>
             <label>x</label>
-            <input type={'number'}/>
+            <input type={'number'} value={customX} onChange={(e) => setCustomX(e.target.value)}/>
             <label>y</label>
-            <input type={'number'}/>
-            <button onClick={() => onClickCustomPositionButton()}>move</button>
+            <input type={'number'} value={customZ} onChange={(e) => setCustomZ(e.target.value)}/>
+            <button onClick={() => handleRobotPosition(customX, customZ)}>move</button>
           </div>
         </div>
       </div>
