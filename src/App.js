f6791f1e8c26cc65635a01397e86172c544189a3
monet2155 <monet9812@gmail.com>
2022-09-15 15:00:58 +0900

    fix: loading screen style

--- src/App.js
+++ src/App.js
@@ -22,15 +22,28 @@ function App() {
   
 
   return (
-    <div style={{flex:1, backgroundColor: '#ccc'}}>
-      {
-        hideSplash && (
-          <div style={{backgroundColor: 'black', width: 1200, height: '800px', position:'absolute', color: 'white', textAlign: 'center'}}>
-            Loading...
-          </div>
-        )
-      }
-      <Unity unityProvider={unityProvider} style={{ width: 1200, height: 800 }} />
+    <div style={{ flex: 1, backgroundColor: "#ccc" }}>
+      {hideSplash && (
+        <div
+          style={{
+            backgroundColor: "black",
+            width: 1200,
+            height: "800px",
+            position: "absolute",
+            color: "white",
+            textAlign: "center",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          Loading...
+        </div>
+      )}
+      <Unity
+        unityProvider={unityProvider}
+        style={{ width: 1200, height: 800 }}
+      />
     </div>
   );
 }
